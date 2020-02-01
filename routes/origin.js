0a17c91dddab60077617ef49633717309e8d7766
Supinic <supinic@protonmail.com>
2020-02-01 22:05:24 +0100

    Refactored origin to use generic-list-table

--- routes/origin.js
+++ routes/origin.js
@@ -6,8 +6,17 @@ module.exports = (function () {
 	const Router = Express.Router();
 
 	Router.get("/", async (req, res) => {
-		res.render("origin", {
-			data: await Origin.list()
+		const data = (await Origin.list()).map(i => ({
+			Emote: i.Emote,
+			Text: i.Text
+		}));
+
+		res.render("generic-list-table", {
+			data,
+			head: Object.keys(data[0]),
+			pageLength: 25,
+			sortColumn: 0,
+			sortDirection: "asc"
 		});
 	});
 
