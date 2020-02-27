29922c9b56b79c055bab65310559946c21cfd119
Supinic <supinic@protonmail.com>
2020-02-27 21:59:41 +0100

    Fixed routing for deprecated endpoints
    Also re-added deprecated API documentation

--- routes/api/bot/index.js
+++ routes/api/bot/index.js
@@ -17,18 +17,44 @@ module.exports = (function () {
 
 	subroutes.forEach(([name, link]) => Router.use("/" + name, require("./" + link)));
 
+	/**
+	 * @api {get} /bot/active Channel bots data
+	 * @apiName GetBotActive
+	 * @apiDescription Deprecated. Use (#Bot-Program:GetBotProgramList) instead.
+	 * @apiGroup Deprecated
+	 * @apiPermission none
+	 * @apiSuccess {number} id Bot's internal ID
+	 * @apiSuccess {string} name Bot's name
+	 * @apiSuccess {string} [prefix] Bot's command prefix. Can be null, if none is used (no commands) or unknown
+	 * @apiSuccess {string} [author] Author's ID. Can be null if unknown
+	 * @apiSuccess {string} [language] Programming language use to implement the bot. Can be null if unknown.
+	 * @apiSuccess {string} [description] Voluntary description
+	 * @apiSuccess {string} [lastSeen] If the bot verifies, this is the date of last verification - as ISO string
+	 * @apiSuccess {number} [lastSeenTimestamp] If the bot verifies, this is the date of last verification - as timestamp
+	 **/
 	Router.get("/active", async (req, res) => {
 		return sb.WebUtils.apiDeprecated(req, res, {
 			original: "/api/bot/active",
-			replacement: "/api/bot-program/active",
+			replacement: "/api/bot-program/bot/list",
 			timestamp: new sb.Date("2020-03-31 23:59:59.999").valueOf()
 		});
 	});
 
+	/**
+	 * @api {put} /bot/active Set channel bot activity
+	 * @apiName UpdateBotActivity
+	 * @apiDescription Deprecated. Use (#Bot-Program:GetBotProgramList) instead.
+	 * @apiGroup Deprecated
+	 * @apiPermission login
+	 * @apiSuccess {boolean} success True if everything was completed successfully
+	 * @apiError (400) InvalidRequest If you authorize correctly, but you're not being tracked as a channel bot
+	 * @apiError (401) Unauthorized Authorization failed
+	 * @apiError (403) AccessDenied Not logged in
+	 **/
 	Router.put("/active", async (req, res) => {
 		return sb.WebUtils.apiDeprecated(req, res, {
 			original: "/api/bot/active",
-			replacement: "/api/bot-program/active",
+			replacement: "/api/bot-program/bot/active",
 			timestamp: new sb.Date("2020-03-31 23:59:59.999").valueOf()
 		});
 	});
