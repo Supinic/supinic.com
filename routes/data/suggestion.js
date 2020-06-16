d4660a0c383f370b4be20d4a84c1097a1e8f46f2
Supinic <supinic@protonmail.com>
2020-06-16 19:43:23 +0200

    fixed the order of suggestion endpoints
    resolves #5

--- routes/data/suggestion.js
+++ routes/data/suggestion.js
@@ -42,6 +42,46 @@ module.exports = (function () {
 		});
 	});
 
+	Router.get("/stats", async (req, res) => {
+		const auth = await sb.WebUtils.getUserLevel(req, res);
+		if (auth.error) {
+			return res.status(401).render("error", {
+				error: "401 Unauthorized",
+				message: "Your session timed out, please log in again"
+			});
+		}
+		else if (!sb.WebUtils.compareLevels(auth.level, "login")) {
+			return res.status(401).render("error", {
+				error: "401 Unauthorized",
+				message: "You must be logged in before viewing your suggestion statistics"
+			});
+		}
+
+		const {Count: totalCount} = (await Suggestion.selectSingleCustom(rs => rs
+			.select("COUNT(*) AS Count")
+		));
+
+		const rawData = await Suggestion.selectMultipleCustom(rs => rs
+			.select("COUNT(*) AS Amount")
+			.where("User_Alias = %n", auth.userID)
+			.groupBy("Status")
+		);
+
+		const userCount = rawData.reduce((acc, cur) => acc += cur.Amount, 0);
+		const data = rawData.map(i => ({
+			Status: i.Status,
+			Count: i.Amount,
+			"% total": sb.Utils.round(i.Amount / totalCount * 100, 2),
+			"% yours": sb.Utils.round(i.Amount / userCount * 100, 2)
+		}));
+
+		res.render("generic-list-table", {
+			data: data,
+			head: Object.keys(data[0]),
+			pageLength: 25
+		});
+	});
+
 	Router.get("/:id", async (req, res) => {
 		const suggestionID = Number(req.params.id);
 		if (!sb.Utils.isValidInteger(suggestionID)) {
@@ -73,46 +113,6 @@ module.exports = (function () {
 					? new sb.Date(data.lastUpdate).format("Y-m-d H:i:s")
 					: "N/A"
 			}
-		});
-	});
-
-	Router.get("/stats", async (req, res) => {
-		const auth = await sb.WebUtils.getUserLevel(req, res);
-		if (auth.error) {
-			return res.status(401).render("error", {
-				error: "401 Unauthorized",
-				message: "Your session timed out, please log in again"
-			});
-		}
-		else if (!sb.WebUtils.compareLevels(auth.level, "login")) {
-			return res.status(401).render("error", {
-				error: "401 Unauthorized",
-				message: "You must be logged in before viewing your suggestions"
-			});
-		}
-
-		const {Count: totalCount} = (await Suggestion.selectSingleCustom(rs => rs
-			.select("COUNT(*) AS Count")
-		));
-
-		const rawData = await Suggestion.selectMultipleCustom(rs => rs
-			.select("COUNT(*) AS Amount")
-			.where("User_Alias = %n", auth.userID)
-			.groupBy("Status")
-		);
-
-		const userCount = rawData.reduce((acc, cur) => acc += cur.Amount, 0);
-		const data = rawData.map(i => ({
-			Status: i.Status,
-			Count: i.Amount,
-			"% total": sb.Utils.round(i.Amount / totalCount * 100, 2),
-			"% yours": sb.Utils.round(i.Amount / userCount * 100, 2)
-		}));
-
-		res.render("generic-list-table", {
-			data: data,
-			head: Object.keys(data[0]),
-			pageLength: 25
 		});
 	});
 
