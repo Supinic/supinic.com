5942b6e7e19675ff11d6e476eb2b53f2da18f854
Supinic <supinic@protonmail.com>
2020-06-21 21:13:35 +0200

    now only shows favourites of the logged in user

--- routes/track/favourite.js
+++ routes/track/favourite.js
@@ -5,14 +5,25 @@ module.exports = (function () {
 	const Router = Express.Router();
 
 	Router.get("/list", async (req, res) => {
+		const { userID } = await sb.WebUtils.getUserLevel(req, res);
+		if (!userID) {
+			return res.status(401).render("error", {
+				error: "401 Unauthorized",
+				message: "You must be logged in before viewing your favourites"
+			});
+		}
+
 		const { data } = await sb.Got.instances.Supinic("track/favourite/list").json();
-		const printData = data.filter(i => i.active).map(i => ({
-			User: i.userName,
-			Track: `<a href="/track/detail/${i.track}">${i.trackName}</a>`
+		const printData = data.filter(i => i.active && i.userAlias === userID).map(i => ({
+			Track: `<a href="/track/detail/${i.track}">${i.trackName}</a>`,
+			Created: new sb.Date(i.created).format("Y-m-d H:i:s"),
+			Edited: (i.lastEdit)
+				? new sb.Date(i.created).format("Y-m-d H:i:s")
+				: "N/A"
 		}));
 
 		res.render("generic-list-table", {
-			head: ["User", "Track"],
+			head: ["Track", "Created", "Edited"],
 			data: printData,
 			pageLength: 25
 		});
