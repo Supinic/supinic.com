eb6625270d6a9f2b4c5b83950a3372df34955676
Supinic <supinic@protonmail.com>
2020-07-26 18:44:49 +0200

    now exposes the amount of favourites in track detail API

--- routes/api/track/detail.js
+++ routes/api/track/detail.js
@@ -5,6 +5,7 @@ module.exports = (function () {
 	const Router = Express.Router();
 
 	const Track = require("../../../modules/track/track.js");
+	const Favourite = require("../../../modules/track/user-favourite.js");
 
 	/**
 	 * @api {get} /track/detail/:id Track - Get detail
@@ -35,28 +36,28 @@ module.exports = (function () {
 	 * @apiSuccess {number} track.relatedTracks.toID
 	 * @apiSuccess {string} track.relatedTracks.name
 	 * @apiSuccess {number} [track.legacyID]
+	 * @apiSuccess {number} track.favourites
 	 * @apiError (400) InvalidRequest ID is out of range<br>
 	 *     ID is out of bounds (does not exist)
 	 */
 	Router.get("/:id", async (req, res) => {
 		const auth = await sb.WebUtils.getUserLevel(req, res);
-		let favourite = null;
-
 		const id = Number(req.params.id);
 		if (!id) {
 			return sb.WebUtils.apiFail(res, 400, "Provided ID is not a valid integer");
 		}
-
-		if (auth.error) {
+		else if (auth.error) {
 			return sb.WebUtils.apiFail(res, 401, auth.error);
 		}
 
 		const trackData = await Track.get(id);
-
 		if (trackData === null) {
 			return sb.WebUtils.apiFail(res, 404, "Track ID not found");
 		}
 		else {
+			const favourites = await Favourite.getForTrack(id);
+			trackData.Favourites = favourites.length;
+
 			return sb.WebUtils.apiSuccess(res, trackData);
 		}
 	});
