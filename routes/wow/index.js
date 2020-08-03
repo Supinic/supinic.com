86406a6bc79ffb7305fc84f017f50057d7610f3f
Supinic <supinic@protonmail.com>
2020-08-03 22:47:31 +0200

    formatting improvements

--- routes/wow/index.js
+++ routes/wow/index.js
@@ -6,7 +6,7 @@ module.exports = (function () {
 
 	const Status = require("../../modules/wow/status.js");
 
-	Router.get("/server/:serverName", async (req, res) => {
+	Router.get("/aq-effort/:serverName", async (req, res) => {
 		const server = req.params.serverName;
 		const data = await Status.getAllLatest(server);
 		if (data.length === 0) {
@@ -16,9 +16,24 @@ module.exports = (function () {
 			});
 		}
 
+		const printData = data.map(i => {
+			const percent = sb.Utils.round(i.Current / i.Required * 100, 2);
+			return {
+				Material: i.Material,
+				Faction: i.Faction,
+				Current: i.Current,
+				Required: i.Required,
+				"%": {
+					value: `${percent}%`,
+					dataOrder: percent
+				},
+				Update: i.Last_Update.format("Y-m-d H:i")
+			};
+		});
+
 		res.render("generic-list-table", {
-			data,
-			head: Object.keys(data[0]),
+			data: printData,
+			head: Object.keys(printData[0]),
 			pageLength: 50,
 			sortColumn: 1,
 			sortDirection: "desc"
