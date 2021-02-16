70c73e152c4792723bc97550a9050c4432548bd9
Supinic <supinic@protonmail.com>
2021-02-16 20:03:36 +0100

    adding various new statistics to the API endpoint

--- routes/api/bot/stats.js
+++ routes/api/bot/stats.js
@@ -4,7 +4,13 @@ module.exports = (function () {
 	const Express = require("express");
 	const Router = Express.Router();
 
+	const firstCommandExecution = new sb.Date("2019-02-28 23:26:36");
+	const oldCommandExecutions = 938178;
+
+	const fetchSizeTables = ["AFK", "Message_Meta_Channel", "Message_Meta_User_Alias", "Reminder", "User_Alias"];
 	const cacheKey = "website-bot-stats";
+
+	const getSize = (data, tableName) => data.find(i => i.Name === tableName).Total;
 
 	Router.get("/", async (req, res) => {
 		const cacheData = await sb.Cache.getByPrefix(cacheKey);
@@ -13,17 +19,25 @@ module.exports = (function () {
 		}
 
 		const [
+			tableSizes,
 			activeChannels,
 			totalUsers,
 			activeUsers,
 			{ Bytes: chatLineSize, Line_Count: chatLines },
 			commands,
+			newCommandExecutions,
 			totalAFKs,
 			activeAFKs,
 			totalReminders,
 			activeReminders,
 			activeFilters
 		] = await Promise.all([
+			sb.Query.getRecordset(rs => rs
+				.select("TABLE_NAME AS Name", "(DATA_LENGTH + INDEX_LENGTH) AS Total")
+				.from("INFORMATION_SCHEMA", "TABLES")
+				.where("TABLE_SCHEMA = %s", "chat_data")
+				.where("TABLE_NAME %s+", fetchSizeTables)
+			),
 			sb.Query.getRecordset(rs => rs
 			    .select("COUNT(*) AS Total")
 			    .from("chat_data", "Channel")
@@ -49,6 +63,12 @@ module.exports = (function () {
 				.select("COUNT(*) AS Total")
 				.from("chat_data", "Command")
 				.where("Flags NOT %*like*", "archived")
+				.single()
+				.flat("Total")
+			),
+			sb.Query.getRecordset(rs => rs
+				.select("COUNT(*) AS Total")
+				.from("chat_data", "Command_Execution")
 				.single()
 				.flat("Total")
 			),
@@ -89,31 +109,43 @@ module.exports = (function () {
 
 		const data = {
 			channels: {
-				active: activeChannels
+				active: activeChannels,
+				meta: {
+					size: getSize(tableSizes, "Message_Meta_Channel")
+				}
 			},
 			users: {
 				active: activeUsers.length,
-				total: totalUsers
+				total: totalUsers,
+				size: getSize(tableSizes, "User_Alias"),
+				meta: {
+					size: getSize(tableSizes, "Message_Meta_User_Alias")
+				}
 			},
 			chatLines: {
 				size: chatLineSize,
 				total: chatLines
 			},
 			commands: {
-				active: commands
+				active: commands,
+				executions: oldCommandExecutions + newCommandExecutions,
+				since: firstCommandExecution
 			},
 			afk: {
 				active: activeAFKs,
-				total: totalAFKs
+				total: totalAFKs,
+				size: getSize(tableSizes, "AFK")
 			},
 			reminders: {
 				active: activeReminders,
 				total: totalReminders,
+				size: getSize(tableSizes, "Reminder")
 			},
 			filters: {
 				active: activeFilters
 			}
 		};
+
 		await sb.Cache.setByPrefix(cacheKey, data, {
 			expiry: 3_600_000
 		});
