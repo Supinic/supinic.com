3e8582177a311b4e522c22aa2c2650a23b6daeb9
Supinic <supinic@protonmail.com>
2021-02-16 20:47:17 +0100

    removed frontend cache, added new properties

--- routes/bot/stats.js
+++ routes/bot/stats.js
@@ -5,52 +5,49 @@ module.exports = (function () {
 	const Express = require("express");
 	const Router = Express.Router();
 
-	const cachePrefix = "web/routes/bot/stats";
 	const stringMap = {
 		channels: {
-			active: "Channels Supibot is active in"
+			active: "Channels Supibot is active in",
+			metaSize: "Size of channel-related meta-data"
 		},
 		users: {
 			active: "Active users in all channels Supibot is in",
-			total: "Total users Supibot encountered so far"
+			total: "Total users Supibot encountered so far",
+			metaSize: "Size of user-related meta-data"
 		},
 		chatlines: {
 			size: "Size the chat line logs take up",
 			total: "Total amount of saved chat lines"
 		},
 		commands: {
-			active: "Amount of commands used since last restart"
+			active: "Currently available commands",
+			countSinceRestart: "Commands excuted since last bot restart",
+			countTotal: "All ever executed commands (since 2019-02-28)",
 		},
 		afk: {
 			active: "Amount of currently AFK users",
-			total: "Total amount of AFK statuses set overall"
+			total: "Total amount of AFK statuses set overall",
+			size: "Size of the AFK status table"
 		},
 		reminders: {
 			active: "Currently pending reminders",
-			total: "Total amount of reminders set overall"
+			total: "Total amount of reminders set overall",
+			size: "Size of the reminder table"
 		},
 		filters: {
-			active: "Amount of bans from Supibot commands, and other filters"
+			active: "Amount of filters (optouts, blocks, whitelists, bans)"
 		}
 	};
 
 	Router.get("/", async (req, res) => {
-		let data = await sb.Cache.getByPrefix(cachePrefix);
-		if (!data) {
-			const apiData = await sb.Got("Supinic", "bot/stats").json();
-
-			data = apiData.data;
-			await sb.Cache.setByPrefix(cachePrefix, data, {
-				expiry: 600_000
-			});
-		}
+		const data = await sb.Cache.getByPrefix(cachePrefix);
 
 		const printData = {};
 		for (const [topKey, topValue] of Object.entries(data)) {
 			for (const [subKey, subValue] of Object.entries(topValue)) {
 				const resultKey = stringMap[topKey][subKey];
 
-				if (topKey === "chatlines" && subKey === "size") {
+				if (subKey === "size" || subKey === "metaSize") {
 					printData[resultKey] = sb.Utils.formatByteSize(subValue);
 				}
 				else {
