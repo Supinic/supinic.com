5df5eec2159795b4a603435dc1eeffaf39ad5937
Supinic <supinic@protonmail.com>
2021-04-08 01:10:18 +0200

    remove Details column - it is now empty and unused

--- routes/data/origin.js
+++ routes/data/origin.js
@@ -11,8 +11,7 @@ module.exports = (function () {
 			return {
 				Emote: `<a href="/data/origin/detail/${i.ID}">${emote}</a>`,
 				Name: i.name,
-				Type: i.type ?? "N/A",
-				Details: ``
+				Type: i.type ?? "N/A"
 			};
 		});
 
