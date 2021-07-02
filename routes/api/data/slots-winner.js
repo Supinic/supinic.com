14ae3d7354a6a178790dfe44095fbbb10c03ca11
Supinic <supinic@protonmail.com>
2021-07-02 18:47:20 +0200

    refactor of `slots-winner` API + frontend endpoints

--- routes/api/data/slots-winner.js
+++ routes/api/data/slots-winner.js
@@ -18,12 +18,13 @@ module.exports = (function () {
 	 * @apiSuccess {number} channel Channel ID, where the slots happened
 	 * @apiSuccess {number} channelName Channel name, where the slots happened
 	 * @apiSuccess {number} odds Odds in format 1:X
-	 * @apiSuccess {string} source Whatever words, numbers the user rolled with
-	 * @apiSuccess {string} result Whatever the slots resulted in
 	 * @apiSuccess {string} timestamp ISO date string of the slots victory
+	 * @apiSuccess {number} rank
 	 **/
 	Router.get("/list", async (req, res) => {
-		const data = await SlotsWinner.selectMultipleCustom(rs => rs
+		const data = await SlotsWinner.selectCustom(rs => rs
+			.select("ID", "User_Alias", "Channel", "Odds", "Timestamp")
+			.select("DENSE_RANK() OVER (ORDER BY Odds DESC) AS Rank")
 			.select("Channel.Name AS Channel_Name")
 			.select("User_Alias.Name AS User_Name")
 			.join("chat_data", "Channel")
@@ -32,6 +33,54 @@ module.exports = (function () {
 
 		return sb.WebUtils.apiSuccess(res, data);
 	});
+
+	/**
+	 * @api {get} /data/slots-winner/list Slots Winner - Detail
+	 * @apiName GetSlotsWinnerDetail
+	 * @apiDescription Fetches the detail of a specific slot winner
+	 * @apiGroup Data
+	 * @apiPermission none
+	 * @apiSuccess {number} ID
+	 * @apiSuccess {number} userAlias
+	 * @apiSuccess {string} userName
+	 * @apiSuccess {number} channel Channel ID, where the slots happened
+	 * @apiSuccess {number} channelName Channel name, where the slots happened
+	 * @apiSuccess {number} odds Odds in format 1:X
+	 * @apiSuccess {string} source Whatever words, numbers the user rolled with
+	 * @apiSuccess {string} result Whatever the slots resulted in
+	 * @apiSuccess {string} timestamp ISO date string of the slots victory
+	 * @apiSuccess {number} rank
+	 **/
+	Router.get("/detail/:id", async (req, res) => {
+		const ID = Number(req.params.ID);
+		if (!sb.Utils.isValidInteger(ID)) {
+			return sb.WebUtils.apiFail(res, 400, "Invalid ID provided");
+		}
+
+		const data = await SlotsWinner.selectSingleCustom(rs => rs
+			.select("Channel.Name AS Channel_Name")
+			.select("User_Alias.Name AS User_Name")
+			.join("chat_data", "Channel")
+			.join("chat_data", "User_Alias")
+			.where("ID = %n", ID)
+			.single()
+		);
+
+		if (!data) {
+			return sb.WebUtils.apiFail(res, 404, "Provided ID does not exist");
+		}
+
+		data.Rank = await SlotsWinner.selectCustom(rs => rs
+			.select("COUNT(*) AS Rank")
+			.join("chat_data", "User_Alias")
+			.where("Odds > %n", data.odds)
+			.groupBy("Odds")
+			.single()
+			.flat("Rank")
+		);
+
+		return sb.WebUtils.apiSuccess(res, data);
+	})
 
 	return Router;
 })();
--- routes/data/slots-winner.js
+++ routes/data/slots-winner.js
@@ -5,26 +5,38 @@ module.exports = (function () {
 	const Router = Express.Router();
 
 	Router.get("/list", async (req, res) => {
-		const { data: rawData } = await sb.Got("Supinic", "/data/slots-winner/list").json();
+		const { data } = await sb.Got("Supinic", "/data/slots-winner/list").json();
 
-		const data = rawData.sort((a, b) => b.odds - a.odds).map(i => ({
-			"Odds - 1:X": sb.Utils.round(i.odds, 3),
+		const printData = rawData.sort((a, b) => b.odds - a.odds).map(i => ({
+			Rank: `<a href="/data/slots-winner/detail/${i.ID}">${i.rank}</a>`,
+			Odds: sb.Utils.round(i.odds, 3),
 			User: i.userName,
 			Channel: i.channelName,
-			Roll: [
-				`<div title="Input: ${i.source.replace(/"/g, `&ldquo;`)}" style="cursor: pointer; text-decoration: underline;">List of words</div>`,
-				`<div title="Winning roll: ${i.result.replace(/"/g, `&ldquo;`)}" style="cursor: pointer; text-decoration: underline;">Winning roll</div>`
-			].join(""),
 			Date: new sb.Date(i.timestamp).format("Y-m-d H:i:s")
 		}));
 
 		res.render("generic-list-table", {
-			data,
+			data: printData,
 			head: Object.keys(data[0]),
-			pageLength: 25,
+			pageLength: 50,
 			sortColumn: 0,
 			sortDirection: "desc"
 		});
+	});
+
+	Router.get("/detail/:id", async (req, res) => {
+		const { data } = await sb.Got("Supinic", `/data/slots-winner/detail/${req.query.id}`).json();
+
+		const printData = {
+			Odds: sb.Utils.round(data.odds, 3),
+			User: data.userName,
+			Channel: data.channelName,
+			Date: new sb.Date(i.timestamp).format("Y-m-d H:i:s"),
+			Roll: sb.Utils.escapeHTML(i.result),
+			Input: sb.Utils.escapeHTML(i.source)
+		};
+
+		res.render("generic-detail-table", { data: printData });
 	});
 
 	return Router;
