7c9f9d37e1b96792cb19d76599e9d38fe9240f89
Supinic <supinic@protonmail.com>
2022-02-15 21:37:42 +0100

    updated session cookie persistence 24h -> 168h

--- app.js
+++ app.js
@@ -120,7 +120,7 @@
 		saveUninitialized: true,
 		cookie: {
 			secure: false,
-			maxAge: 24 * 60 * 60 * 1000
+			maxAge: 7 * 864e5
 		},
 		store: new MySQLStore({
 			user: process.env.MARIA_USER,
