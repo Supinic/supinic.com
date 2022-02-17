01c31d1765d5950f330f1c8a80b16a0a0758aa6d
Supinic <supinic@protonmail.com>
2022-02-17 20:16:29 +0100

    changed data source to `null`, added title

--- routes/data/suggestion.js
+++ routes/data/suggestion.js
@@ -8,7 +8,7 @@ module.exports = (function () {
 		all: ["Author", "Text", "Status", "Priority", "Update", "ID"],
 		active: ["ID", "Text", "Status", "Priority", "Update"],
 		resolved: ["ID", "Text", "Status", "Priority", "Update"],
-		clientside:  ["Author", "Text", "Status", { data: "Update", type: "timeDelta" }, "ID"]
+		clientside:  ["Author", "Text", "Status", { data: null, title: "Update", type: "timeDelta" }, "ID"]
 	};
 
 	const redirect = async (req, res, urlCallback) => {
