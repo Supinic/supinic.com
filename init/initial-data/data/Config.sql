e050c99e80b9cfe8d68538a7926966c840e7b87a
EuleHaken <sven220ce@gmail.com>
2022-05-19 17:52:03 +0200

    SOUNDCLOUD_CLIENT_ID has to be editable - apparently

--- init/initial-data/data/Config.sql
+++ init/initial-data/data/Config.sql
@@ -5,7 +5,7 @@ VALUES
 ('ADMIN_USER_NAME','REPLACE_ME','string',NULL,0,0,NULL),
 ('DEFAULT_USER_AGENT','REPLACE_ME','string',NULL,0,0,NULL),
 ('LOG_USER_CRON','*/10 * * * * *','string',NULL,0,0,'Determines how often newly detected users should be added in a batch.'),
-('SOUNDCLOUD_CLIENT_ID','REPLACE_ME','string',NULL,1,0,NULL),
+('SOUNDCLOUD_CLIENT_ID','REPLACE_ME','string',NULL,1,1,NULL),
 ('TWITCH_APP_ACCESS_TOKEN','REPLACE_ME','string',NULL,1,0,NULL),
 ('TWITCH_CLIENT_ID','REPLACE_ME','string',NULL,1,0,NULL),
 ('VIDEO_TYPE_REPLACE_PREFIX','$','string',NULL,0,0,NULL),
