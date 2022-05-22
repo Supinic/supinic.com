fc392a11d7caa43092420193f96d7d695cd03c47
EuleHaken <sven220ce@gmail.com>
2022-05-22 16:19:07 +0200

    smol typo fix

--- init/script.js
+++ init/script.js
@@ -25,7 +25,7 @@ const config = {
 		"chat_data/tables/Platform",
 		"chat_data/tables/Channel",
 		"chat_data/tables/Filter",
-		"chat_data/tables/Comamnd_Execution",
+		"chat_data/tables/Command_Execution",
 
 		"data/database",
 		"data/tables/Config"
