5fcf5cfcc07934c0635d04009211768532398455
Supinic <supinic@protonmail.com>
2022-06-15 17:52:04 +0200

    added server-side replacement of newlines

--- modules/data/dall-e.js
+++ modules/data/dall-e.js
@@ -5,10 +5,16 @@ module.exports = (function () {
 
 	class DallE extends TemplateModule {
 		static async getImages (id) {
-			return await this.selectCustom(q => q
-				.select("Data", "Created", "Creation_Time")
+			const data = await this.selectCustom(q => q
+				.select("Data AS Images", "Created", "Creation_Time")
 				.where("ID = %s", id)
 			);
+
+			for (const row of data) {
+				row.Images = row.Images.replace(/\\n/g, "");
+			}
+
+			return data;
 		}
 
 		static get name () { return "dalle"; }
