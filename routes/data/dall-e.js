2850fd44f9757ee77c82c04991cb3e31a46c3dca
Supinic <supinic@protonmail.com>
2022-06-16 15:53:37 +0200

    added client-side caching of existing image set IDs

--- routes/data/dall-e.js
+++ routes/data/dall-e.js
@@ -4,14 +4,29 @@ module.exports = (function () {
 	const Express = require("express");
 	const Router = Express.Router();
 
+	const existingCache = new Set();
+
 	Router.get("/detail/:id", async (req, res) => {
 		const { id } = req.params;
-		const response = await sb.Got("Supinic", `data/dall-e/detail/${id}/exists`);
-		if (!response.body.data.exists) {
-			return res.status(404).render("error", {
-				error: sb.WebUtils.formatErrorMessage(404),
-				message: "Image set does not exist"
-			});
+
+		if (!existingCache.has(id)) {
+			const response = await sb.Got("Supinic", `data/dall-e/detail/${id}/exists`);
+
+			if (!response.body.data) {
+				return res.status(503).render("error", {
+					error: sb.WebUtils.formatErrorMessage(503),
+					message: "API is currently overloaded, try again latert"
+				});
+			}
+			else if (!response.body.data.exists) {
+				return res.status(404).render("error", {
+					error: sb.WebUtils.formatErrorMessage(404),
+					message: "Image set does not exist"
+				});
+			}
+			else {
+				existingCache.add(id);
+			}
 		}
 
 		res.render("dall-e-display", {
