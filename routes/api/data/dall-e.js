6553a2a10d0b7532bc72f3d2b1ff18171509665b
Supinic <supinic@protonmail.com>
2022-06-17 16:40:48 +0200

    added API and frontend route for image preview
    - should be used in OGP tags later

--- routes/api/data/dall-e.js
+++ routes/api/data/dall-e.js
@@ -15,6 +15,26 @@ module.exports = (function () {
 		return sb.WebUtils.apiSuccess(res, data);
 	});
 
+	Router.get("/detail/:id/preview/:index", async (req, res) => {
+		const index = Number(req.params.index);
+		if (!sb.Utils.isValidInteger(index)) {
+			return sb.WebUtils.apiFail(res, 400, "Malformed index provided");
+		}
+		else if (index < 0 || index >= 8) {
+			return sb.WebUtils.apiFail(res, 400, "Index out of bounds <0..8>");
+		}
+
+		const data = await DallE.getImages(req.params.id);
+		if (!data) {
+			return sb.WebUtils.apiFail(res, 404, "Image set does not exist");
+		}
+
+		return sb.WebUtils.apiSuccess(res, {
+			index,
+			image: data.Images[index] ?? null
+		});
+	});
+
 	Router.get("/detail/:id/exists", async (req, res) => {
 		const exists = await DallE.existsCustom(q => q.where("ID = %s", req.params.id));
 		return sb.WebUtils.apiSuccess(res, { exists });
--- routes/data/dall-e.js
+++ routes/data/dall-e.js
@@ -15,7 +15,7 @@ module.exports = (function () {
 			if (!response.body.data) {
 				return res.status(503).render("error", {
 					error: sb.WebUtils.formatErrorMessage(503),
-					message: "API is currently overloaded, try again latert"
+					message: "API is currently overloaded, try again later"
 				});
 			}
 			else if (!response.body.data.exists) {
@@ -33,6 +33,28 @@ module.exports = (function () {
 			id,
 			title: "DALL-E images"
 		});
+	});
+
+	Router.get("/detail/:id/preview/:index", async (req, res) => {
+		const { id, index } = req.params;
+
+		const response = await sb.Got("Supinic", `data/dall-e/detail/${id}/preview/${index}`);
+		if (response.statusCode === 400) {
+			return res.status(400).render("error", {
+				error: sb.WebUtils.formatErrorMessage(404),
+				message: "Invalid parameters provided"
+			});
+		}
+
+		const { image } = response.body.data;
+		const imageBuffer = Buffer.from(image, "base64");
+
+		res.writeHead(200, {
+			"Content-Type": "image/png",
+			"Content-Length": imageBuffer.length
+		});
+
+		res.end(imageBuffer);
 	});
 
 	return Router;
