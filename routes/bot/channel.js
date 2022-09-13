41900a76afc6eac564c5eb95fce060bdc452c513
Supinic <supinic@protonmail.com>
2022-09-13 17:52:35 +0200

    added a check for since-deleted aliases

--- routes/bot/channel.js
+++ routes/bot/channel.js
@@ -169,7 +169,9 @@ module.exports = (function () {
 		const printData = response.body.data.map(alias => {
 			const created = (alias.created) ? new sb.Date(alias.created) : null;
 			const link = `<a href="/bot/user/${encodeURIComponent(alias.linkAuthor)}/alias/detail/${encodeURIComponent(alias.linkName)}">🔗 ${alias.name}</a>`
-			const invocation = `<code>(link to alias ${alias.linkName} made by ${alias.linkAuthor})</code>`;
+			const invocation = (alias.linkName && alias.linkAuthor)
+				? `<code>(link to alias ${alias.linkName} made by ${alias.linkAuthor})</code>`
+				: `<code>(deleted alias)</code>`;
 
 			return {
 				Name: {
