e52ca19c34ea1cf6cfa68f60004e629b1341c7e5
Supinic <supinic@protonmail.com>
2023-02-02 17:30:55 +0100

    added a `FakeAgent` got definition

---
+++ got/fake-agent.mjs
@@ -0,0 +1,7 @@
+export const definition = {
+	responseType: "text",
+	throwHttpErrors: false,
+	headers: {
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+	}
+};
--- got/index.mjs
+++ got/index.mjs
@@ -1,8 +1,10 @@
+import { definition as FakeAgent } from "./fake-agent.mjs";
 import { definition as Global } from "./global.mjs";
 import { definition as Supibot } from "./supibot.mjs";
 import { definition as Supinic } from "./supinic.mjs";
 
 const definitions = [
+	FakeAgent,
 	Global,
 	Supibot,
 	Supinic
