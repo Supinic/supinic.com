a195a71731a3dce38a1f83fae809b4eef7053ada
Supinic <supinic@protonmail.com>
2023-02-02 17:42:13 +0100

    fixed erroneous got definition

--- got/fake-agent.mjs
+++ got/fake-agent.mjs
@@ -1,7 +1,13 @@
 export const definition = {
-	responseType: "text",
-	throwHttpErrors: false,
-	headers: {
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-	}
+	name: "FakeAgent",
+	optionsType: "function",
+	options: {
+		responseType: "text",
+		throwHttpErrors: false,
+		headers: {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+		}
+	},
+	parent: null,
+	description: "Pretends to be a browser request, mostly used for scraping"
 };
