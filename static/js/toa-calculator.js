c264b655e0340e01dbd100ccc2e4d7691b88b466
Supinic <supinic@protonmail.com>
2023-04-21 17:56:06 +0200

    removed logs, minor cleanup

--- static/js/toa-calculator.js
+++ static/js/toa-calculator.js
@@ -10,55 +10,55 @@ const findInvocationEl = (name) => document.querySelector(`li.invocation[name="$
 const recalculateSummary = () => {
 	const { attributes, base, constants, invocationCategories, modes } = RaidsData;
 	const invocations = fullInvocationList.filter(i => i.selected);
-	
+
 	let level = base.level;
-	for (const invocation of invocations) {		
+	for (const invocation of invocations) {
 		level += invocation.level;
 	}
-	
+
 	const uncapped = level * constants.percentagePerRaidLevel;
 	const capped = Math.min(uncapped, constants.damagePercentageCap);
 	const mode = modes.find(i => (i.min <= level && level <= i.max));
-	
+
 	const summaryEl = document.getElementById("summary");
-	
-	const iconEl = summaryEl.querySelector(`li[summary=icon]`);	
+
+	const iconEl = summaryEl.querySelector(`li[summary=icon]`);
 	iconEl.style.backgroundImage = `url(${mode.icon})`;
-	
-	const levelEl = summaryEl.querySelector(`li[summary=level]`);	
+
+	const levelEl = summaryEl.querySelector(`li[summary=level]`);
 	levelEl.textContent = `Level: ${level}`;
-	
+
 	for (const attribute of attributes) {
 		const attributeEl = summaryEl.querySelector(`li[summary=${attribute.name}]`);
 		const percentage = (attribute.capped) ? capped : uncapped;
 		attributeEl.textContent = `${attribute.name}: +${percentage}%`;
 	}
+
 	setHash();
 };
 
-// This is used in importInvocations to prevent recursive updates
+// Used in `importInvocations` to prevent recursive updates
 let urlUpdatesLocked = false;
-
 const setHash = () => {
 	if (urlUpdatesLocked) {
 		return;
 	}
-	// Update URL...
+
 	const invos = generateInvocations();
 	const url = new URL(location.toString());
+
 	url.hash = invos;
-	console.log(`Updating URL hash from ${location.hash} to ${url.hash}`);
-	window.history.replaceState(invos, /* unused: */null, url);
+	window.history.replaceState(invos, null, url);
 };
 
 const generateInvocations = () => {
 	const selectedInvocations = fullInvocationList.filter(i => i.selected);
-	
+
 	let result = 0n;
 	for (const invocation of selectedInvocations) {
 		result += (1n << (BigInt(invocation.id) - 1n));
 	}
-	
+
 	return result.toString(16).padStart(12, "0");
 };
 
@@ -81,25 +81,26 @@ const importInvocations = (hash = null) => {
 		alert(`Invalid value provided: ${rawValue}`);
 		return;
 	}
-	
+
 	const value = BigInt(numValue);
 	if (value < 0n || value > (1n << BigInt(fullInvocationList.length))) {
 		alert("Value is outside of working range");
 		return;
 	}
-	
+
 	urlUpdatesLocked = true;
 	clearInvocations();
-	
+
 	for (const invocation of fullInvocationList) {
 		const mask = 1n << (BigInt(invocation.id) - 1n);
 		if ((value & mask) === 0n) {
 			continue;
 		}
-		
+
 		const el = findInvocationEl(invocation.name);
 		el.click();
 	}
+
 	urlUpdatesLocked = false;
 	setHash();
 };
@@ -123,12 +124,12 @@ window.addEventListener("load", () => {
 	for (const category of RaidsData.invocationCategories) {
 		const { title, unique, boss, icon, list } = category;
 		const titleEl = document.createElement("li");
-		titleEl.textContent = title;		
+		titleEl.textContent = title;
 		topListEl.appendChild(titleEl);
-		
-		const categoryListEl = document.createElement("ul");	
+
+		const categoryListEl = document.createElement("ul");
 		const listItems = [];
-		
+
 		for (const invocation of list) {
 			const { name, description, level, summary, requires, data } = invocation;
 
@@ -138,9 +139,9 @@ window.addEventListener("load", () => {
 			listItemEl.setAttribute("fullName", name);
 			listItemEl.textContent = `${name}: ${summary} +${level}`;
 
-			listItemEl.addEventListener("select", function (evt) { 
+			listItemEl.addEventListener("select", function (evt) {
 				invocation.selected = evt.detail.selected;
-				
+
 				if (evt.detail.selected === false) {
 					const name = this.getAttribute("fullName");
 					const dependents = fullInvocationList.filter(i => i.requires?.includes(name));
@@ -153,11 +154,11 @@ window.addEventListener("load", () => {
 						}
 					}
 				}
-				
+
 				recalculateSummary();
-			});	
-			
-			listItemEl.addEventListener("otherUnselect", function (evt) { 
+			});
+
+			listItemEl.addEventListener("otherUnselect", function (evt) {
 				if (!Array.isArray(requires)) {
 					return;
 				}
@@ -167,16 +168,16 @@ window.addEventListener("load", () => {
 				else {
 					this.classList.remove("selected");
 				}
-			});			
-			
-			if (Array.isArray(requires)) {			
-				listItemEl.addEventListener("click", function () {					
+			});
+
+			if (Array.isArray(requires)) {
+				listItemEl.addEventListener("click", function () {
 					const requiredEls = requires
 						.map(i => document.querySelector(`li[name=${normalize(i)}]`))
 						.filter(Boolean);
-				
+
 					const missing = requiredEls.filter(i => !i.classList.contains("selected"));
-					if (this.classList.contains("selected") || missing.length === 0) {						
+					if (this.classList.contains("selected") || missing.length === 0) {
 						const selected = this.classList.toggle("selected");
 						const selectEvent = new CustomEvent("select", { detail: { selected } });
 						this.dispatchEvent(selectEvent);
@@ -184,23 +185,23 @@ window.addEventListener("load", () => {
 					else {
 						const missingNames = missing.map(i => i.getAttribute("fullName"));
 						alert(`Requires: ${missingNames.join(", ")}`);
-					}				
+					}
 				});
 			}
 			else {
-				listItemEl.addEventListener("click", function () { 
+				listItemEl.addEventListener("click", function () {
 					const selected = this.classList.toggle("selected");
 					const selectEvent = new CustomEvent("select", { detail: { selected } });
 					this.dispatchEvent(selectEvent);
 				});
 			}
-			
+
 			categoryListEl.appendChild(listItemEl);
 		}
-		
-		for (const listItemEl of categoryListEl.children) {				
+
+		for (const listItemEl of categoryListEl.children) {
 			if (unique) {
-				listItemEl.addEventListener("click", function () { 
+				listItemEl.addEventListener("click", function () {
 					const others = Array.from(categoryListEl.children).filter(i => i !== this);
 					for (const el of others) {
 						el.classList.remove("selected");
@@ -208,9 +209,9 @@ window.addEventListener("load", () => {
 						el.dispatchEvent(selectEvent);
 					}
 				});
-			}		
+			}
 		}
-		
+
 		topListEl.appendChild(categoryListEl);
 	}
 
@@ -223,7 +224,7 @@ window.addEventListener("load", () => {
 	// This may set the hash but as long as it was parsed by importInvocations above it's fine
 	recalculateSummary();
 
-	
+
 	document.getElementById("button-clear").addEventListener("click", () => clearInvocations());
 	document.getElementById("button-import")
 		.addEventListener("click",
@@ -237,7 +238,7 @@ window.addEventListener("load", () => {
 				alert(generateInvocations());
 			}
 		);
-	
+
 });
 
 window.addEventListener("hashchange", () => {
