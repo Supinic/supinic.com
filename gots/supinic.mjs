d0286fa9faf9a329846c2e060144cceb94c46ad5
Supinic <supinic@protonmail.com>
2023-09-28 13:09:11 +0200

    use the proper temporary IP

--- gots/supinic.mjs
+++ gots/supinic.mjs
@@ -2,7 +2,7 @@ export const definition = {
 	name: "Supinic",
 	optionsType: "object",
 	options: {
-		prefixUrl: "http://192.168.0.143/api",
+		prefixUrl: "http://192.168.0.154/api", // @todo move this to some kind of config
 		timeout: {
 			request: 30000
 		}
