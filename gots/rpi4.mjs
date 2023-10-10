4c50af73c64163ee1f0e4c818c4cc3f6ebe3b9a2
Supinic <supinic@protonmail.com>
2023-10-10 15:56:31 +0200

    update temporary IPs back to regular values

--- gots/rpi4.mjs
+++ gots/rpi4.mjs
@@ -2,7 +2,7 @@ export const definition = {
 	name: "RPi4",
 	optionsType: "object",
 	options: {
-		prefixUrl: "http://192.168.0.154:11111/proxy",
+		prefixUrl: "http://localhost:11111/proxy",
 		timeout: {
 			request: 10000
 		}
--- gots/supinic.mjs
+++ gots/supinic.mjs
@@ -2,7 +2,7 @@ export const definition = {
 	name: "Supinic",
 	optionsType: "object",
 	options: {
-		prefixUrl: "http://192.168.0.154/api", // @todo move this to some kind of config
+		prefixUrl: "http://localhost/api", // @todo move this to some kind of config
 		timeout: {
 			request: 30000
 		}
