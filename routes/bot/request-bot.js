6e12b8b75c71c0ae200dff3834dbad051a20db49
Supinic <supinic@protonmail.com>
2023-12-19 19:35:04 +0100

    updated christmas holiday break for 2023/24

--- routes/bot/request-bot.js
+++ routes/bot/request-bot.js
@@ -85,8 +85,8 @@ module.exports = (function () {
 		}
 
 		const now = sb.Date.now();
-		const christmasHolidayEnd = "2023-01-03";
-		const isChristmasHoliday = (new sb.Date("2022-12-22") < now && now < new sb.Date("2023-01-02"));
+		const christmasHolidayEnd = "2024-01-01";
+		const isChristmasHoliday = (new sb.Date("2023-12-20") < now && now < new sb.Date("2024-01-01"));
 
 		res.render("generic-form", {
 			prepend: sb.Utils.tag.trim `
