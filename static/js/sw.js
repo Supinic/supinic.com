3fd988dcfc773b9f3f4301d6a24e506b845e1b81
supinic <supinic@protonmail.com>
2025-06-19 12:22:05 +0200

    add debug code and take over new versions immediately

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,4 +1,12 @@
+self.addEventListener("install", (evt) => console.log("Imgur redirector: Installed", { evt });
+self.addEventListener("activate", (evt) => {
+	console.log("Imgur redirector: Activated", { evt });
+	evt.waitUntil(self.clients.claim());
+}
+
 self.addEventListener("fetch", (evt) => {
+	console.log("Imgur redirector: Fetch", { evt });
+
 	const url = new URL(evt.request.url);
 	if (url.hostname !== "i.imgur.com") {
 		// everything else goes through
