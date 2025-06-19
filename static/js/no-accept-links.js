7344689323b7893ad09ab31a9b4759ed43ddf2f2
supinic <supinic@protonmail.com>
2025-06-19 13:01:37 +0200

    typo fix in accept header remover

--- static/js/no-accept-links.js
+++ static/js/no-accept-links.js
@@ -4,7 +4,7 @@ const noAcceptLinks = document.querySelectorAll("a.noaccept");
 for (const el of noAcceptLinks) {
 	el.addEventListener("click", async (evt) => {
 		evt.preventDefault();
-		const res = await fetch(a.href, { headers: { Accept: "" }});
+		const res = await fetch(el.href, { headers: { Accept: "" } });
 		const blob = await res.blob();
 
 		const url = URL.createObjectURL(blob);
