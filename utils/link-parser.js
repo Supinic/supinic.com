42d24883fcf2aaa6b44613a357ba65ad2261c70e
supinic <supinic@protonmail.com>
2026-09-11 16:28:04 +0200

    fix invalid access to importable module class

--- utils/link-parser.js
+++ utils/link-parser.js
@@ -37,7 +37,7 @@ const getLinkParser = async () => {
 			console.log("No Bilibili configuration, skipping Bilibili parser");
 		}
 
-		linkParser = new LinkParser(options);
+		linkParser = new LinkParser.default(options);
 	}
 
 	return linkParser;
